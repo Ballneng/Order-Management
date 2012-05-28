038bf62cddf7c5da16a39d32cdd3b24ce98d366d
Ball wang <337855696@qq.com>
2012-05-28 17:40:46 +0800

    custmer address can pull

--- protected/modules/ballwang/controllers/OrderController.php
+++ protected/modules/ballwang/controllers/OrderController.php
@@ -188,31 +188,182 @@ class OrderController extends BallController {
 
     protected function getOrder($db) {
         $dbConnectString = '';
-        $row='';
-        //  $dbConnect = mysql_connect($db->site_db_host, $db->site_db_name, $db->site_db_password);
-        $dbConnect = mysql_connect("localhost", "root", "");
+        $row = '';
+        $dbConnect = mysql_connect($db->site_db_host, $db->site_db_name, $db->site_db_password);
+
         if (!$dbConnect) {
             $dbConnectString .= $db->site_db_name . 'connect failed!';
         } else {
-            mysql_select_db('ui', $dbConnect) or die('Query interrupted' . mysql_error());
+            mysql_select_db($db->site_db_name, $dbConnect) or die('Query interrupted' . mysql_error());
             mysql_query("SET NAME 'UTF8'");
-            $sql = "select t1.*,t2.*,t3.customer_email,t4.carrier_name,t5.response_txn_id
+            $sql = "select t1.*,t2.*,t3.*,t4.carrier_name,t4.carrier_id,t5.response_txn_id
                 from syo_order AS t1
                 LEFT JOIN  syo_customer_address  AS t2 ON t1.order_address_id=t2.address_id
                 LEFT JOIN syo_customer AS t3 ON t1.customer_id=t3.customer_id
                 LEFT JOIN syo_carrier AS t4 ON t1.order_carrier_id=t4.carrier_id
                 LEFT JOIN syo_paypal_response AS t5 ON t1.order_id=t5.order_id
-                WHERE t1.order_valid=1 AND  (t1.order_status=" . Order::PaymentAccepted . " OR t1.order_status=" . Order::AwaitingPayment . " ) AND t1.order_export=0
+                WHERE t1.order_valid=1 AND  (t1.order_status=" . Order::PaymentAccepted . " OR t1.order_status=" . Order::Shipped . " ) AND t1.order_export=0
                 ORDER BY order_payment_at ASC";
             $query = mysql_query($sql);
-            while($row = mysql_fetch_array($query)){
-                
+            while ($row = mysql_fetch_array($query)) {
+                var_dump($row['address_city']);
+                echo '<br></br>';
             }
-
             echo $dbConnectString . '######';
         }
-        
+
         mysql_close($dbConnect);
+    }
+
+    /**
+     * 同步订单
+     * @param type $row
+     * @param type $type 
+     */
+    protected function setSynOrderList($row, $type='syo', $db) {
+        
+    }
+
+    protected function synOrderListUpdate($row, $db) {
+        $error = '';
+        $success ='';
+        $isOrder = Order::model()->findByAttributes(array('order_site_id' => $db->site_id, 'invoice_id' => $row['invoice_id']));
+        if ($isOrder) {
+            $order = $isOrder;
+        } else {
+            $order = new Order();
+        }
+        $order->order_site_id = $db->site_id;
+        $order->invoice_id = $row['invoice_id'];
+        $custmerId = (int) $row['customer_id'];
+
+        $userAndAddress=  $this->setUser($row, $db);
+        $order->customer_id = $userAndAddress['userId'];                                //change
+        $order->order_subtotal = $row['order_subtotal'];
+        $order->order_trackingtotal = $row['order_trackingtotal'];
+        $order->order_promo_free = $row['order_promo_free'];
+        $order->order_grandtotal = $row['order_grandtotal'];
+        $order->order_discount_id = $row['order_discount_id'];
+        $order->order_currency_id = $row['order_currency_id'];
+        $order->order_currency_id = $row['order_currency_id'];
+        $order->order_carrier_id = $row['order_carrier_id'];                      //change
+        $order->order_address_id = $userAndAddress['addressId'];                      //change
+        $order->order_status = $row['order_status'];
+        $order->order_valid = $row['order_valid'];
+        $order->order_export = $row['order_export'];
+        $order->order_qty = $row['order_qty'];
+        $order->order_ip = $row['order_ip'];
+        $order->order_salt = $row['order_salt'];
+        $order->order_comment = $row['order_comment'];
+        $order->order_create_at = $row['order_create_at'];
+        $order->order_payment_at = $row['order_payment_at'];
+        if ($order->save()) {
+            
+        } else {
+            $error .=$db->site_prefix . $row['invoice_id'] . '订单同步失败！';
+        }
+    }
+
+    /**
+     *用户账号和地址信息录入
+     * @param type $row
+     * @param type $db
+     * @return array() 返回为数组附带用户ID，地址ID，成功信息，失败信息 
+     */
+    protected function setUser($row, $db) {
+        $isAddressExist = 0;
+        $addressReturnId = 0;
+        $error = '';
+        $success = '';
+        $userId = 0;
+        $return = array();
+        $user = Customer::model()->findByAttributes(array('customer_email' => $row['customer_email']));
+        if ($user) {
+            //如有变动则更新
+            $userString = md5($user->customer_name . $user->customer_active . $user->customer_role . $user->customer_default_address . $user->customer_group . $user->customer_visit_count);
+            $rowUserString = md5($row['customer_name'] . $row['customer_active'] . $row['customer_role'] . $row['customer_default_address'] . $row['customer_group'] . $row['customer_visit_count']);
+            if ($userString != $rowUserString) {
+                $user->customer_name = $row['customer_name'];
+                $user->customer_active = $row['customer_active'];
+                $user->customer_role = $row['customer_role'];
+                $user->customer_default_address = $row['customer_default_address'];
+                $user->customer_group = $row['customer_group'];
+                $user->customer_visit_count = $row['customer_visit_count'];
+                if ($user->update()) {
+                    $success .= $db->site_prefix . $user->customer_name . '用户信息更新成功！';
+                }
+            }
+            //检查地址是否存在
+            $address = $user->address;
+            foreach ($address as $key => $value) {
+                if ($value->customer_firstname == $row['customer_firstname'] && $value->customer_lastname == $row['customer_lastname'] && $value->customer_name == $row['customer_name'] && $value->address_street == $row['address_street'] && $value->address_city == $row['address_city'] && $value->address_state == $row['address_state'] && $value->address_country == $row['address_country']) {
+                    $addressReturnId = $value->address_id;
+                    $isAddressExist = 1;
+                }
+            }
+            if (!$isAddressExist) {
+                $address = new CustomerAddress();
+                $address->customer_id = $user->customer_id;
+                $address->customer_gender = $row['customer_gender'];
+                $address->customer_firstname = $row['customer_firstname'];
+                $address->customer_lastname = $row['customer_lastname'];
+                $address->address_company = $row['address_company'];
+                $address->address_street = $row['address_street'];
+                $address->address_city = $row['address_city'];
+                $address->address_state = $row['address_state'];
+                $address->address_country = $row['address_country'];
+                $address->address_postcode = $row['address_postcode'];
+                $address->address_phonenumber = $row['address_phonenumber'];
+                $address->address_create_at = $row['address_create_at'];
+                if ($address->save()) {
+                    $addressReturnId = $address->address_id;
+                } else {
+                    $error .=$db->site_prefix . $row['customer_name'] . ' 存在账号但地址保存失败！<br>';
+                }
+            }
+        } else {
+            $user = new Customer();
+            $user->customer_email = $row['customer_email'];
+            $user->customer_name = $row['customer_name'];
+            $user->customer_pwd = $row['customer_pwd'];
+            $user->customer_newsletter = $row['customer_newsletter'];
+            $user->customer_active = $row['customer_active'];
+            $user->customer_role = $row['customer_role'];
+            $user->customer_default_address = $row['customer_default_address'];
+            $user->customer_group = $row['customer_group'];
+            $user->customer_ip = $row['customer_ip'];
+            $user->customer_login = $row['customer_login'];
+            $user->customer_visit_count = $row['customer_visit_count'];
+            $user->customer_last_update = $row['customer_last_update'];
+            $user->customer_create_at = $row['customer_create_at'];
+            if ($user->save()) {
+                $address = new CustomerAddress();
+                $address->customer_id = $user->customer_id;
+                $address->customer_gender = $row['customer_gender'];
+                $address->customer_firstname = $row['customer_firstname'];
+                $address->customer_lastname = $row['customer_lastname'];
+                $address->address_company = $row['address_company'];
+                $address->address_street = $row['address_street'];
+                $address->address_city = $row['address_city'];
+                $address->address_state = $row['address_state'];
+                $address->address_country = $row['address_country'];
+                $address->address_postcode = $row['address_postcode'];
+                $address->address_phonenumber = $row['address_phonenumber'];
+                $address->address_create_at = $row['address_create_at'];
+                if ($address->save()) {
+                    $addressReturnId = $address->address_id;
+                } else {
+                    $error .=$db->site_prefix . $row['customer_name'] . ' 地址保存失败！<br>';
+                }
+            } else {
+                $error .= $db->site_prefix . $row['customer_email'] . '账号保存失败！<br>';
+            }
+        }
+        $return['success'] = $success;
+        $return['error'] = $error;
+        $return['addressId'] = $addressReturnId;
+        $return['userId'] = $userId;
+        return $return;
     }
 
 }
--- protected/modules/ballwang/models/CustomerAddress.php
+++ protected/modules/ballwang/models/CustomerAddress.php
@@ -1,9 +1,9 @@
 <?php
 
 /**
- * This is the model class for table "{{customer_address}}".
+ * This is the model class for table "{{CustomerAddress}}".
  *
- * The followings are the available columns in table '{{customer_address}}':
+ * The followings are the available columns in table '{{CustomerAddress}}':
  * @property integer $address_id
  * @property integer $customer_id
  * @property integer $customer_gender
@@ -23,7 +23,6 @@ class CustomerAddress extends CActiveRecord
 {
 	/**
 	 * Returns the static model of the specified AR class.
-	 * @param string $className active record class name.
 	 * @return CustomerAddress the static model class
 	 */
 	public static function model($className=__CLASS__)
@@ -47,15 +46,15 @@ class CustomerAddress extends CActiveRecord
 		// NOTE: you should only define rules for those attributes that
 		// will receive user inputs.
 		return array(
-			array('customer_id, customer_gender, customer_firstname, customer_lastname, customer_name, address_street, address_city, address_state, address_country, address_postcode, address_phonenumber, address_create_at', 'required'),
-			array('customer_id, customer_gender, address_country', 'numerical', 'integerOnly'=>true),
-			array('customer_firstname, customer_lastname, address_company, address_city, address_state', 'length', 'max'=>64),
-			array('customer_name', 'length', 'max'=>128),
-			array('address_street', 'length', 'max'=>255),
+			array('customer_id, customer_firstname, customer_lastname, address_street, address_city, address_state, address_country, address_postcode, address_phonenumber', 'required'),
+			array('customer_id, customer_gender,address_country', 'numerical', 'integerOnly'=>true),
+			array('customer_firstname, customer_lastname, address_company,address_city, address_state', 'length', 'max'=>64),
+            array('customer_name','length','max'=>64),
+            array('address_street','length','max'=>255),
 			array('address_postcode, address_phonenumber', 'length', 'max'=>32),
 			// The following rule is used by search().
 			// Please remove those attributes that should not be searched.
-			array('address_id, customer_id, customer_gender, customer_firstname, customer_lastname, customer_name, address_company, address_street, address_city, address_state, address_country, address_postcode, address_phonenumber, address_create_at', 'safe', 'on'=>'search'),
+			array('address_id, customer_id, customer_gender, customer_firstname, customer_lastname,customer_name, address_company, address_street, address_city, address_state, address_country, address_postcode, address_phonenumber, address_create_at', 'safe', 'on'=>'search'),
 		);
 	}
 
@@ -67,6 +66,8 @@ class CustomerAddress extends CActiveRecord
 		// NOTE: you may need to adjust the relation name and the related
 		// class name for the relations automatically generated below.
 		return array(
+            'country'=>array(self::BELONGS_TO,'Country','address_country'),
+            'customer'=>array(self::BELONGS_TO,'Customer','customer_id'),
 		);
 	}
 
@@ -79,16 +80,15 @@ class CustomerAddress extends CActiveRecord
 			'address_id' => 'Address',
 			'customer_id' => 'Customer',
 			'customer_gender' => 'Customer Gender',
-			'customer_firstname' => 'Customer Firstname',
-			'customer_lastname' => 'Customer Lastname',
-			'customer_name' => 'Customer Name',
-			'address_company' => 'Address Company',
-			'address_street' => 'Address Street',
-			'address_city' => 'Address City',
-			'address_state' => 'Address State',
-			'address_country' => 'Address Country',
-			'address_postcode' => 'Address Postcode',
-			'address_phonenumber' => 'Address Phonenumber',
+			'customer_firstname' => 'Firstname',
+			'customer_lastname' => 'Lastname',
+			'address_company' => 'Company',
+			'address_street' => 'Street',
+			'address_city' => 'City',
+			'address_state' => 'State',
+			'address_country' => 'Country',
+			'address_postcode' => 'Postcode',
+			'address_phonenumber' => 'Phonenumber',
 			'address_create_at' => 'Address Create At',
 		);
 	}
@@ -103,24 +103,50 @@ class CustomerAddress extends CActiveRecord
 		// should not be searched.
 
 		$criteria=new CDbCriteria;
+        
 
 		$criteria->compare('address_id',$this->address_id);
+
 		$criteria->compare('customer_id',$this->customer_id);
+
 		$criteria->compare('customer_gender',$this->customer_gender);
+
 		$criteria->compare('customer_firstname',$this->customer_firstname,true);
+
 		$criteria->compare('customer_lastname',$this->customer_lastname,true);
-		$criteria->compare('customer_name',$this->customer_name,true);
+
+        $criteria->compare('customer_name',$this->customer_name,true);
+
 		$criteria->compare('address_company',$this->address_company,true);
+
 		$criteria->compare('address_street',$this->address_street,true);
+
 		$criteria->compare('address_city',$this->address_city,true);
+
 		$criteria->compare('address_state',$this->address_state,true);
+
 		$criteria->compare('address_country',$this->address_country);
+
 		$criteria->compare('address_postcode',$this->address_postcode,true);
+
 		$criteria->compare('address_phonenumber',$this->address_phonenumber,true);
+
 		$criteria->compare('address_create_at',$this->address_create_at,true);
 
-		return new CActiveDataProvider($this, array(
+		return new CActiveDataProvider('CustomerAddress', array(
 			'criteria'=>$criteria,
 		));
 	}
+
+    public function  beforeSave()
+    {
+        $this->customer_name = $this->customer_firstname.' '.$this->customer_lastname;
+        return true;
+    }
+
+    public static function getGender($id=0)
+    {
+        $gender = array(1 => 'Mr', 2 => 'Miss', 3 => 'Mrs', 4 => 'Ms');
+        return  $id==0  ? $gender[1] : $gender[$id];
+    }
 }
